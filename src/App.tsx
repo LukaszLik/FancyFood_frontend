0e14907093126a3376d340c3ecc1dbec887db459
WLadyga <wladyga@gmail.com>
2021-04-05 17:51:42 +0200

    added app colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "./Colors.css";
+import { ThemeProvider, Button } from "@material-ui/core";
+import theme from './theme';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" style = {{backgroundColor: "var(--white)"}}/>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code style = {{color: "var(--secondary_d)"}}>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +26,7 @@ function App() {
         </a>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#ffc107"
+      },
+      secondary: {
+        main: "#26c6da"
+      }
+    },
+});
+
+export default theme;
