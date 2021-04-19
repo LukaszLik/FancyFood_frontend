c231b0051ad7cc98ab55dcaff4a7f77a1ac2ff40
Metanefrydia <mgregula10@gmail.com>
2021-04-19 23:07:12 +0200

    Added register page

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./Colors.css";
-import { ThemeProvider, Button } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core";
 import theme from "./thems";
 
 ReactDOM.render(
