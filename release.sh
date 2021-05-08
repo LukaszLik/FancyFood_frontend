581862b2a1fbf1495e88472c0b4a64f88d8f7f19
W.Ladyga <wladyga@gmail.com>
2021-05-08 21:42:15 +0200

    v 1.0.1

--- release.sh
+++ release.sh
@@ -1,5 +1,1 @@
-git add . &&
-git commit -m "v $1" &&
-git tag -a $1 -m "release $1" &&
-git push &&
-git push --tags
+git add . && git commit -m "v $1" && git tag -a $1 -m "release $1" && git push && git push --tags
